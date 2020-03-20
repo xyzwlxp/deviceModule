0f23b9dfe221386f129deaa77acb1bc7067f64fd
Piao <xyzwlxp@gmail.com>
2020-03-20 16:09:57 +0800

    Fix one.c it should use raw_

--- one.c
+++ one.c
@@ -33,7 +33,7 @@ int onebyte_release(struct inode *inode, struct file *filep)
 ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
 {
     /*please complete the function on your own*/
-    copy_to_user(buf, onebyte_data, 1);
+    raw_copy_to_user(buf, onebyte_data, 1);
     if (*f_pos > 0)
     {
         return 0;
